39650f3ab814984f039439a72e78965e22053eca
Daniel <danielchristofolli@gmail.com>
2021-12-17 20:02:47 -0300

    added facade test and jacoco and sonarqube tasks

--- src/test/java/com/dchristofolli/sicredichallenge/v1/facade/AssemblyFacadeTest.java
+++ src/test/java/com/dchristofolli/sicredichallenge/v1/facade/AssemblyFacadeTest.java
@@ -1,7 +1,28 @@
 package com.dchristofolli.sicredichallenge.v1.facade;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.dchristofolli.sicredichallenge.v1.service.AgendaService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static com.dchristofolli.sicredichallenge.Stub.agendaEntityStub;
+import static com.dchristofolli.sicredichallenge.Stub.agendaRequestStub;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
 class AssemblyFacadeTest {
+    @Mock
+    AgendaService agendaService;
+    @InjectMocks
+    AssemblyFacade assemblyFacade;
 
+    @Test
+    void createAgenda() {
+        when(agendaService.save(agendaRequestStub())).thenReturn(agendaEntityStub());
+        assemblyFacade.createAgenda(agendaRequestStub());
+        assertEquals(agendaEntityStub(), agendaService.save(agendaRequestStub()));
+    }
 }
