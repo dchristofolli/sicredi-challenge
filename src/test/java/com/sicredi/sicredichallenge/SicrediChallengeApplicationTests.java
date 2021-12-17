79de1445c5e56aa76eefe8734f164253d588a137
Daniel <danielchristofolli@gmail.com>
2021-12-17 16:24:14 -0300

    Springboot project started with main dependencies

---
+++ src/test/java/com/sicredi/sicredichallenge/SicrediChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sicredi.sicredichallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SicrediChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
